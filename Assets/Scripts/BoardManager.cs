df62b1784fd3693ee5e1063523ca9cc592e7e7da
Liam Dyer <liamdyer@Bills-MacBook-Pro.local>
2015-09-21 23:52:03 -0400

    Draws sprite onto center of screen

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+	public GameObject unit;
+	private Unit selected = null; // The currently selected unit
+
+	private Transform boardHolder; 
+
+	public void WasSelected(Unit selectedUnit) {
+		selected = selectedUnit;
+	}
+
+	// Currently just creates one unit and adds it to the center of the map
+	public void SetupScene() {
+		// Place the unit in the center of the Camera
+		Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10f));
+		GameObject instance =
+			Instantiate (unit, centerPos, Quaternion.identity) as GameObject;
+		instance.transform.SetParent (boardHolder);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	private BoardManager boardManager; 
+
+	void Awake () {
+		// Enforce singleton-ness
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		boardManager = GetComponent<BoardManager> ();
+		boardManager.SetupScene ();
+	
+	}
+}
