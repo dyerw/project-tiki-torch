b416046129f90f74d44673a95014913a22024370
Liam Dyer <liamdyer@Bills-MacBook-Pro.local>
2015-09-24 17:31:53 -0400

    Unit range displayed as circle around unit

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,14 +5,13 @@ public class BoardManager : MonoBehaviour {
 
 	public GameObject unit;
 
-	private Transform boardHolder; 
-
 	// Currently just creates one unit and adds it to the center of the map
 	public void SetupScene() {
 		// Place the unit in the center of the Camera
 		Vector3 centerPos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10f));
 		GameObject instance =
 			Instantiate (unit, centerPos, Quaternion.identity) as GameObject;
-		instance.transform.SetParent (boardHolder);
+
+
 	}
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class Unit : MonoBehaviour {
 
+	public GameObject range; // The visual representation of the units range
 	public Sprite selectedSprite;
 	public Sprite unselectedSprite;
 	public int movementRange;
 
 	private SpriteRenderer spriteRenderer;
 	private bool isSelected;
+	private GameObject rangeInstance;
 
 	void OnMouseUpAsButton() {
 		UpdateAppearance (true);
@@ -27,6 +29,13 @@ public class Unit : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		spriteRenderer = GetComponent<SpriteRenderer>();
+
+		// Add the range indicator
+		rangeInstance = Instantiate (range, transform.position, Quaternion.identity) as GameObject;
+
+		// Scale it to represent the movementRange accurately
+		float rangeScale = movementRange * 2;
+		rangeInstance.transform.localScale = new Vector3 (rangeScale, rangeScale, 0f);
 	}
 	
 	// Update is called once per frame
@@ -35,9 +44,11 @@ public class Unit : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0) && isSelected) {
 
 			Vector3 newPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			newPos = new Vector3(newPos.x, newPos.y, 0f);
 
 			if (isInMovementRange(newPos)) {
-				transform.position = new Vector3 (newPos.x, newPos.y, 0f);
+				transform.position = newPos;
+				rangeInstance.transform.position = newPos;
 			}
 		
 		// Right click deselects unit
